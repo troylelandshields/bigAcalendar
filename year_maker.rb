0266b8a7ed161edd214a92c7c8527449e18ebafb
tshields <troy.leland.shields@gmail.com>
2014-01-26 21:34:31 -0700

    Created and added year_maker.

---
+++ year_maker.rb
@@ -0,0 +1,47 @@
+require 'RMagick'
+require '/Users/troyshields/Documents/Ruby/calendar/month_maker.rb'
+include Magick
+
+
+def test!
+	yearCreator = YearCreator.new
+	yearCreator.createYear(2014, 3)
+end
+
+
+class YearCreator
+
+	attr_accessor
+		:months
+
+	def initialize
+		@monthDays = {
+			"JANUARY" => 31,
+			"FEBRUARY" => 28,
+			"MARCH" => 31,
+			"APRIL" => 30,
+			"MAY" => 31,
+			"JUNE" => 30,
+			"JULY" => 31,
+			"AUGUST" => 31,
+			"SEPTEMBER" => 30,
+			"OCTOBER" => 31,
+			"NOVEMBER" => 30,
+			"DECEMBER" => 31
+		}
+	end
+
+	def createYear(year, startDay)
+		monthMaker = MonthMaker.new
+		@monthDays.keys.each {|month|
+			puts "Creating #{month}..."
+			monthMaker.createMonth(startDay, @monthDays[month], month, 2014)
+			startDay = (startDay + @monthDays[month])%7
+			puts "#{month} finished."
+		}
+
+		
+	end
+end
+
+test! if __FILE__==$0
